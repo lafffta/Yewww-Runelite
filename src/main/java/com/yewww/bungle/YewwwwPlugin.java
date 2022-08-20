89a65fae4d07420890307d2c83d109dd7b98c1c3
Aidan Bjelke <aidanbjelke@gmail.com>
2022-08-19 23:38:30 -0600

    Comments and revised logic

--- src/main/java/com/yewww/bungle/YewwwwPlugin.java
+++ src/main/java/com/yewww/bungle/YewwwwPlugin.java
@@ -4,7 +4,6 @@ import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.events.GameObjectDespawned;
-import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -44,9 +43,9 @@ public class YewwwwPlugin extends Plugin
 		}
 		catch (UnsupportedAudioFileException | IOException | LineUnavailableException e)
 		{
-			log.warn("Unable to load builtin Yewww sound", e);
+			log.warn("Unable to load builtin Yewww sound 1", e);
 		}
-
+//		try (InputStream fileStream = new BufferedInputStream(YewwwwPlugin.class.getResourceAsStream("YOUUUU.wav"));
 		try (InputStream fileStream = new BufferedInputStream(new FileInputStream(new File("src/main/resources", "YOUUUU.wav")));
 			 AudioInputStream sound = AudioSystem.getAudioInputStream(fileStream))
 		{
@@ -55,7 +54,7 @@ public class YewwwwPlugin extends Plugin
 		}
 		catch (UnsupportedAudioFileException | IOException | LineUnavailableException e)
 		{
-			log.warn("Unable to load builtin Yewww sound", e);
+			log.warn("Unable to load builtin Yewww sound 2", e);
 		}
 	}
 
@@ -64,8 +63,8 @@ public class YewwwwPlugin extends Plugin
 		log.info("Yewww stopped!");
 	}
 
-//	static final int[] yew_ids = {YEW, NULL_10823, YEW_36683, YEW_40756};
-	static final int[] yew_ids = {TREE, TREE_1277, TREE_1278, TREE_1279, TREE_1280, TREE_40750, TREE_40752};
+//	static final int[] yew_ids = {YEW, NULL_10823, YEW_36683, YEW_40756}; // yew ids
+	static final int[] yew_ids = {TREE, TREE_1277, TREE_1278, TREE_1279, TREE_1280, TREE_40750, TREE_40752}; // normal tree ids (DEBUG)
 
 	@Subscribe
 	public void onGameObjectDespawned(final GameObjectDespawned event)
@@ -73,15 +72,16 @@ public class YewwwwPlugin extends Plugin
 		final GameObject object = event.getGameObject();
 		int id = object.getId();
 		for (int yew_id : yew_ids) {
-			if ((id == yew_id) && (object.getWorldLocation().distanceTo2D(client.getLocalPlayer().getWorldLocation()) <= 5)) {
-//				client.playSoundEffect(SoundEffectID.GE_DECREMENT_PLOP, SoundEffectVolume.HIGH); // play sound
-				double rand = Math.random();
-				if((rand < 0.01) && config.rareSound()) {
-					rareClip.setFramePosition(0);
-					rareClip.start();
-				} else {
-					clip.setFramePosition(0);
-					clip.start();
+			if(object.getWorldLocation().distanceTo2D(client.getLocalPlayer().getWorldLocation()) <= 5) { // if close to player
+				if (id == yew_id) { // if object id is a yew tree
+					double rand = Math.random(); // 0.0 to 1.0
+					if ((rand < 0.01) && config.rareSound()) { // 1% chance of rare sound
+						rareClip.setFramePosition(0);
+						rareClip.start();
+					} else {
+						clip.setFramePosition(0);
+						clip.start();
+					}
 				}
 			}
 		}
